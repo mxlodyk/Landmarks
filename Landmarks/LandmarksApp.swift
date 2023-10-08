d07cab0e77db6790ae25d2577ee19c94aa78b492
Melody Flavel <melodykimfl@gmail.com>
2023-10-08 14:12:40 +0800

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Melody Flavel on 8/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
